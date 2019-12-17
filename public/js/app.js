e3b5ecb34b18897924a062fe68723b238dbb9556
Tiago Santos <santos.tiago@gmail.com>
2019-12-17 16:51:50 +0000

    Set up Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-fetch('https://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-one')
@@ -19,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error)
